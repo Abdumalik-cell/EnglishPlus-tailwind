5313a5842a05c50ecb19b71ac794552224a887ec
Abdumalik-cell <chamyd1966@gmail.com>
2026-02-04 22:33:48 +0500

    add full site

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 const mobileMenuBtn = document.getElementById('mobile-menu-btn');
 const mobileMenu = document.getElementById('mobile-menu');
+const modal = document.getElementById('modal');
+const btn = document.getElementById('btn');
+const overlay = document.getElementById('modalOverlay');
 
 mobileMenuBtn.addEventListener('click', () => {
 	mobileMenu.classList.toggle('hidden');
@@ -10,4 +13,51 @@ mobileMenuBtn.addEventListener('click', () => {
 	} else {
 		icon.setAttribute('d', 'M6 18L18 6M6 6l12 12');
 	}
+});
+
+function openModal() {
+	overlay.classList.remove('hidden');
+	document.body.style.overflow = 'hidden';
+
+	setTimeout(() => {
+		modal.classList.remove('scale-95', 'opacity-0');
+		modal.classList.add('scale-100', 'opacity-100');
+	}, 10);
+}
+
+function closeModal() {
+	modal.classList.add('scale-95', 'opacity-0');
+
+	setTimeout(() => {
+		overlay.classList.add('hidden');
+		document.body.style.overflow = '';
+	}, 300);
+}
+
+overlay.addEventListener('click', e => {
+	if (e.target === overlay) {
+		closeModal();
+	}
+});
+
+document.addEventListener('keydown', e => {
+	if (e.key === 'Escape') {
+		closeModal();
+	}
+});
+
+const form = overlay.querySelector('form');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+
+	const data = {
+		fullName: document.getElementById('fullName').value,
+		phoneNumber: document.getElementById('phoneNumber').value,
+		emailAddress: document.getElementById('emailAddress').value,
+	};
+
+	console.log(data);
+
+	closeModal();
 });
